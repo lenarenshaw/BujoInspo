7e3bca5204eb4d1fd5f29a6f5740c7f0980edf94
Lena Renshaw <lena.renshaw@gmail.com>
2017-09-16 10:18:38 -0400

    Initial Commit

---
+++ BujoInspo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BujoInspo
+//
+//  Created by Lena Renshaw on 9/16/17.
+//  Copyright © 2017 Lena Renshaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
